d2859bea8a7154eabfad4417dc685321df468eff
recoma <seokbong60@gmail.com>
2025-09-07 00:41:49 +0900

    refactor; page 부분에 use client 삭제

--- src/app/guide-generator/page.tsx
+++ src/app/guide-generator/page.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import GpxParserForm from '@/components/guide-generator/GpxParserForm';
 import TrailSegmentEditor from '@/components/guide-generator/TrailSegmentEditor';
 import CourseEditor from '@/components/guide-generator/CourseEditor';
