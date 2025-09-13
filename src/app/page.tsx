4547584fe42ff56a7cec6836d361f147889e19d9
recoma <seokbong60@gmail.com>
2025-09-13 19:14:40 +0900

    feat: 메인 페이지 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,25 @@
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+
 const Home = () => {
   return (
-    <div>
-      <h1>hello world</h1>
+    <div className="h-screen flex items-center justify-center p-4">
+      <div className="w-3/5 max-w-xl">
+        <div className="text-center">
+          <div className="mb-10">
+            <h1 className="text-4xl font-bold">Trail Course Guide</h1>
+            <p className="mt-2 text-md text-gray-500">하이킹/트레킹 코스 관련 가이드 페이지</p>
+          </div>
+          <div className="flex justify-between">
+            <Button asChild size="lg">
+              <Link href="/guide-generator">코스 가이드 만들기</Link>
+            </Button>
+            <Button disabled size="lg">
+              코스 검색하기
+            </Button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
